418d239f27caec9599b6920b88f528a6d1006df3
Lwin <lwindev97@gmail.com>
2025-07-23 11:54:59 +0630

    Custom component and CSS styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Post from "./components/Post";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <main>
+      <Post author="Jenny" body="A cute writer with the bright mindset." />
+      <Post author="Kook" body="Who wrote about musics and the notes." />
+      <Post author="JK" body="More than a writer... Public speaker." />
+    </main>
+  );
 }
 
 export default App;
