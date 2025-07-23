a3e329791dcdec73ae08e0982950f1160915bd7c
Lwin <lwindev97@gmail.com>
2025-07-23 13:56:48 +0630

    Using useState hook

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
+
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
+  const [enteredAuthor, setEnteredAuthor] = useState();
+  const [enteredBody, setEnteredBody] = useState();
+
+  function changeAuthorHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  function changeBodyHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onAuthorChange={changeAuthorHandler}
+        onBodyChange={changeBodyHandler}
+      />
       <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Jenny" body="A cute writer with the bright mindset." />
         <Post author="Kook" body="Who wrote about musics and the notes." />
         <Post author="JK" body="More than a writer... Public speaker." />
