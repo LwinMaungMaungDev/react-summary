0ca55bf225ef9e0b79058e2a3b519ffb216a3960
Lwin <lwindev97@gmail.com>
2025-07-23 13:40:30 +0630

    Inputs form to enter new post

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,17 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Jenny" body="A cute writer with the bright mindset." />
-      <Post author="Kook" body="Who wrote about musics and the notes." />
-      <Post author="JK" body="More than a writer... Public speaker." />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Jenny" body="A cute writer with the bright mindset." />
+        <Post author="Kook" body="Who wrote about musics and the notes." />
+        <Post author="JK" body="More than a writer... Public speaker." />
+      </ul>
+    </>
   );
 }
 
