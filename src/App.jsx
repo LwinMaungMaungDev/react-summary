fce4d005d688f4a75cf16560501984377b3b0f9d
Lwin <lwindev97@gmail.com>
2025-07-23 12:12:49 +0630

    Another custom component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import Post from "./components/Post";
+import PostsList from "./components/PostsList";
 
 function App() {
   return (
     <main>
-      <Post author="Jenny" body="A cute writer with the bright mindset." />
-      <Post author="Kook" body="Who wrote about musics and the notes." />
-      <Post author="JK" body="More than a writer... Public speaker." />
+      <PostsList />
     </main>
   );
 }
---
+++ src/components/PostsList.jsx
@@ -0,0 +1,14 @@
+import Post from "./Post";
+import classes from "./PostsList.module.css";
+
+function PostsList() {
+  return (
+    <ul className={classes.posts}>
+      <Post author="Jenny" body="A cute writer with the bright mindset." />
+      <Post author="Kook" body="Who wrote about musics and the notes." />
+      <Post author="JK" body="More than a writer... Public speaker." />
+    </ul>
+  );
+}
+
+export default PostsList;
