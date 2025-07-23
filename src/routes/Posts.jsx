e5bda00dbad71f3299862a3d4cf61f594618efa2
Lwin <lwindev97@gmail.com>
2025-07-23 16:31:20 +0630

    Data Fetching via loader()s

--- src/routes/Posts.jsx
+++ src/routes/Posts.jsx
@@ -13,3 +13,9 @@ function Posts() {
 }
 
 export default Posts;
+
+export async function loader() {
+  const res = await fetch("http://localhost:8080/posts");
+  const data = await res.json();
+  return data.posts;
+}
